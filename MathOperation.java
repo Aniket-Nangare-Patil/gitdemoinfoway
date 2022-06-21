c0d381caa42f6e6c064ccb3a4752c7f3d6ffb464
Aniket-Nangare-Patil <aniketapatil2016@gmail.com>
2022-06-21 10:04:53 +0530

    p

---
+++ MathOperation.java
@@ -0,0 +1,61 @@
+package session11;
+//Q.Create a class MathOperation with two data member X and Y to store the operand and third data member R to store result of operation.
+import java.util.Scanner;
+public class MathOperation {
+	public class Mathoperation 
+	{
+	   private int X;
+	   private int Y;
+	   private int R;
+	   
+	   public void init()
+	   {
+		      Scanner sc = new Scanner(System.in);
+		      System.out.println("Enter first number::");
+		      X = sc.nextInt();
+		      System.out.println("Enter second number::");
+		      Y = sc.nextInt();
+	   }
+	   
+	   public int add()
+	   {
+		   return R = (int)(X+Y);
+	   }
+	   public int multiply()
+	   {
+		   return R = (int)(X*Y);
+	   }
+	   public int power()
+	   {
+		   long Result=1;
+		   while(Y!=0)
+		   {
+			   Result *= X;
+			   --Y; 
+		   }
+		   return R=(int)Result;
+		   
+		   
+	   }
+	   public void display()
+	   {
+		   System.out.println("First number ="+X +" Second number ="+Y+" Answer ="+ R);
+	   }
+	   
+	   public void main(String[] args) 
+	   {
+		   Mathoperation m = new Mathoperation();
+		   m.init();
+		   
+		   m.add();
+		   m.display();
+		   
+		   m.multiply();
+		   m.display();
+		   
+		   m.power();
+		   m.display();
+	   }
+	}
+}
+	
